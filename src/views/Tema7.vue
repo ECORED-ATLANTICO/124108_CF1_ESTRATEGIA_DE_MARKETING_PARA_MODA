f28acfc27eeb4b5e025d8fb436f814caa047b6c0
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-03-08 23:58:27 -0500

    tema 7

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -7,6 +7,201 @@
         span 7
       h1 Marco legal vigente, e-commerce y redes sociales
 
+    img(data-aos="fade-up")(src="@/assets/curso/tema7/1.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text.banner-text--azul
+          p.mb-0 Se puede plantear el comercio electrónico, o e-commerce, en su definición más amplia, #[strong como el proceso de comercialización (compra y venta) de productos por medios electrónicos, utilizando aplicaciones móviles e Internet]; o como una acción empresarial para mejorar sus ventas a través del uso de las nuevas tecnologías. 
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema7/2.png", alt="imagen de acompañamiento")
+      .col-lg-8
+        .p-4.bg-azul-claro.rounded-3.mb-3
+          p.mb-0 Según Rincón (2017), #[strong el comercio electrónico es una metodología inteligente para hacer negocios, la cual es capaz de detectar necesidades de empresas, comerciantes y consumidores]. Asimismo, ayuda a la reducción de costos de operación y desplazamientos de los intervinientes en la relación comercial, además de mejorar el tiempo de entrega de los bienes, ya sean estos productos o servicios. 
+        p.mb-0 Igualmente, para la Ley 517 de 1999, en su artículo 2º, #[strong el comercio electrónico abarca las cuestiones suscitadas por toda relación de índole comercial, sea o no contractual, estructurada a partir de la utilización de uno o más mensajes de datos o de cualquier otro medio similar]. Las relaciones de índole comercial comprenden, sin limitarse a ellas, las siguientes operaciones: toda operación comercial de suministro o intercambio de bienes o servicios; todo acuerdo de distribución; toda operación de representación o mandato comercial; todo tipo de operaciones financieras, bursátiles y de seguros; de construcción de obras; de consultoría; de ingeniería; de concesión de licencias; todo acuerdo de concesión o explotación de un servicio público; de empresa conjunta y otras formas de cooperación industrial o comercial; de transporte de mercancías o de pasajeros, por vía aérea, marítima, férrea, o por carretera. 
+
+      Separador
+      
+      #t_7_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+        h2 7.1 Generalidades del comercio electrónico
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.bg-terciario-t20.rounded-3
+      .col-lg-9.p-3.p-xl-0
+        p.mb-4.mb-lg-0 El comercio electrónico tiene sus cimientos en el año 1920, cuando diversas empresas mayoristas generaron catálogos impresos, donde incluían imágenes e información de sus productos. Este material permitía que las personas desde sus casas escogieran dichos productos para su compra (Silva Murillo, 2009). Posterior a esto, #[strong con la llegada del computador y el Internet se generaron nuevas rutas para conectar las empresas con su público objetivo], rompiendo barreras de espacio haciendo uso de las diferentes herramientas desarrolladas junto a los avances tecnológicos.
+      .col-8.col-sm-6.col-lg-2
+        figure
+          img.mb-4.mb-lg-0(src="@/assets/curso/tema7/3.png", alt="Imagen decorativa" data-aos="fade-left")
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema7/4.png", alt="imagen de acompañamiento")
+      .col-lg-8
+        p.mb-0 El e-commerce surge como una manera de cubrir la demanda de negocios globales y hace parte de la economía mundial. Se considera #[strong fundamental para el desarrollo empresarial a nivel internacional e, incluso a nivel social], está definiendo con fuerza, pues permite eliminar barreras entre países con el intercambio de bienes y servicios, mediante un contacto totalmente digitalizado, en tiempo real, entre un proveedor y un cliente con fines netamente comerciales, lo que posibilita altas penetraciones a nivel global. Existen varias modalidades de comercio electrónico. Es de destacar que se pueden definir cuatro tipos de comercio electrónico (Silva Murillo, 2009, p.158-161), como se aprecia a continuación.
+
+    //- TODO: Tarjetas pendientes
+
+    Separador
+    
+    #t_7_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 7.2 Características del comercio electrónico
+
+    p.mb-4.mb-lg-5 El comercio electrónico se caracteriza por varios elementos.
+
+    .row.justify-content-center.align-items-center.factores-slider.z-2
+      .d-none.d-xl-block.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema7/9.png", alt="Imagen de acompañamiento")
+      .col-xl-8
+        SlyderF(columnas="col-lg-6")
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/10.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Ubicuidad
+            p.mb-4.mb-5.text-center No se requiere lugar físico para la realización de transacciones. Internet posibilita disponibilidad horaria, de espacio y de momento, es decir, la red de Internet está disponible en todos lados.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/11.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Alcance global
+            p.mb-4.mb-5.text-center Posibilita la configuración de un mercado con transacciones comerciales sin límites.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/12.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Interactividad
+            p.mb-4.mb-5.text-center En aras de satisfacer las necesidades del cliente, el comercio electrónico genera estrategias de interacción permanente entre comerciante y usuario.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/13.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Personalización
+            p.mb-4.mb-5.text-center A través de una segmentación, se puede contactar al consumidor y ajustar el mensaje con sus intereses específicos.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/14.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Adecuación
+            p.mb-4.mb-5.text-center Con base en las preferencias o comportamiento del usuario, se pueden generar cambios de producto o servicio.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/15.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Tecnología social
+            p.mb-4.mb-5.text-center Posibilidad de que el usuario genere contenidos que pueden ser compartidos por medio de canales de internet o redes sociales.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/16.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Densidad de la información
+            p.mb-4.mb-5.text-center Disponibilidad de la información para clientes, comerciantes y participantes del mercado, en igual condición, posibilitando intercambios permanentes.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/17.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Estándares universales
+            p.mb-4.mb-5.text-center Implementación de plataformas y métodos estandarizados que reducen costos y esfuerzos de búsqueda para los consumidores.
+          .tarjeta.tarjeta--blanca.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img(src='@/assets/curso/tema7/18.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Riqueza
+            p.mb-4.mb-5.text-center La web permite, durante la venta de bienes y servicios, transmitir mensajes, textos, imágenes, videos que generan interactividad y riqueza.
+
+    Separador
+
+    #t_7_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 7.3 Regulación del comercio electrónico
+
+    .row.justify-content-center
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema7/19.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3 Abstención absoluta de intromisión legislativa en el ciberespacio.
+            p.mb-0 Se afirma que el ciberespacio es un escenario con autonomía y sin jurisdicción del Estado.
+
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema7/20.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3 Autorregulación a partir del marco propiciado por los sujetos involucrados.
+            p.mb-0 Son códigos de conducta derivados de la participación de los grupos, gremios o asociaciones suministradoras de acceso a Internet.
+
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema7/21.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3 Regulación legislativa basada en las normativas actuales.
+            p.mb-0 
+
+    Separador
+    
+    .row.justify-content-center
+      .col-xl-10
+        #t_7_4.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+          h2 7.4 Normatividad del comercio electrónico
+
+    img(data-aos="fade-up")(src="@/assets/curso/tema7/22.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text.banner-text--azul
+          p.mb-0 En materia de e-commerce en Colombia, la normatividad tiene como marco jurídico la Ley 527 de 1999 y el decreto 1747 de 2000. Estas son algunas declaraciones regulatorias. 
+
+    .bg-linea-tiempo.mb-6(:style="{'--bg-image': `url(${require('@/assets/curso/common/bg-tiempo.png')})`}")
+      LineaTiempoC.color-secundario.mt-5.bg-white.z-2.p-4.rounded-4
+        .row.justify-content-center.align-items-center(titulo="Ley 527 de 1999")
+          .col-lg-7.col-xl-6
+            h5 Ley 527 de 1999
+            p.mb-4.mb-lg-0 Es la encargada de regular las transacciones en la red y brindar el soporte jurídico para la realización de las operaciones por medios electrónicos. El uso de los mensajes de datos, las firmas digitales para todas las actividades, así como la costumbre mercantil en materia de e-commerce están reglamentados a partir de esta ley.
+          .col-10.col-lg-5.col-xl-4
+            figure
+              img(src="@/assets/curso/tema7/23.png", alt="Imagen de acompañamiento")
+
+        .row.justify-content-center.align-items-center(titulo="Decreto 1747 de 2000")
+          .col-lg-7.col-xl-6
+            h5 Decreto 1747 de 2000
+            p.mb-4.mb-lg-0 Reglamenta parcialmente la ley 527 de 1999, en lo relacionado con las entidades de certificación, los certificados y las firmas digitales. Está fundamentado en los principios de la Comisión de Naciones Unidas para el Derecho Mercantil Internacional (CNUDMI). Este decreto, aunque no tiene definida explícitamente reglamentación sobre la protección del consumidor, sí cita el estatuto 1480 del 2011 que protege a los consumidores, buscando nivelar al consumidor, desde una perspectiva legal, con las condiciones de ventaja que tienen comercializadores y productores.
+          .col-10.col-lg-5.col-xl-4
+            figure
+              img(src="@/assets/curso/tema7/24.png", alt="Imagen de acompañamiento")
+
+        .row.justify-content-center.align-items-center(titulo="CONPES 3620 de 2009")
+          .col-lg-7.col-xl-6
+            h5 CONPES 3620 de 2009
+            p.mb-4.mb-lg-0 Este documento de política pública contiene los lineamientos de política para el desarrollo e impulso del e- commerce en Colombia y se incorpora en el plan de acción de la protección de datos personales; recomienda promover y aplicar la ley 1266 de 2008, que es la que dicta las disposiciones generales del habeas data y regula el manejo de la información en sectores diferentes al comercial y financiero, a nivel nacional e internacional.
+          .col-10.col-lg-5.col-xl-4
+            figure
+              img(src="@/assets/curso/tema7/25.png", alt="Imagen de acompañamiento")
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.bg-terciario-t20.rounded-3
+      .col-lg-9.p-3.p-xl-0
+        p.mb-4.mb-lg-0 A pesar de que hay un piso normativo para el comercio electrónico, Colombia sigue teniendo vacíos jurídicos, que pueden perjudicar la aplicación de este modelo y generar molestias en la relación entre el comprador y la plataforma, poniendo en riesgo el nivel de satisfacción y, por ende, las ventas. A continuación, se enuncian algunos de dichos vacíos.
+      .col-8.col-sm-6.col-lg-2
+        figure
+          img.mb-4.mb-lg-0(src="@/assets/curso/tema7/26.png", alt="Imagen decorativa" data-aos="fade-left")
+
+    
 
 </template>
 
