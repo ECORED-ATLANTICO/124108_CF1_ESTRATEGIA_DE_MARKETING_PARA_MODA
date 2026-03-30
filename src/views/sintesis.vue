0fef447e576292d0576303d670cc95ccda40858d
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-03-30 14:06:46 -0500

    Ajustado sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,11 +2,18 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
-    .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+    .row.justify-content-center.align-items-center.mb-5
+      .col-10.col-sm-8.col-lg-4
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/1.png", alt="")
+      .col-lg-8
+        p El componente formativo 01 ofrece al aprendiz una comprensión inicial de los fundamentos del mercadeo y su importancia en el desarrollo de estrategias orientadas al mercado. A partir del análisis del concepto de mercadeo, su evolución y sus principales enfoques, el componente permite reconocer distintos tipos de mercadeo y su aplicación en contextos organizacionales. Asimismo, aborda la relevancia del mercadeo en la generación de valor, el posicionamiento y la sostenibilidad de las empresas en entornos competitivos.
+        .p-4.bg-azul-claro.rounded-4
+          p.mb-0 Adicionalmente, el componente presenta las variables del mercado a través de la mezcla de mercadeo, así como los elementos que intervienen en la formulación de un plan estratégico de mercadeo y el análisis de la competencia. Finalmente, se introduce el comercio electrónico y su marco legal vigente, destacando su papel en las dinámicas actuales de comercialización, de esta manera, el aprendiz fortalece su comprensión del entorno del mercadeo y su relación con los escenarios digitales.
+    .row.justify-content-center
+      .col-12 
+        figure.mb-4
+          img(src="@/assets/curso/sintesis.svg", alt="")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
